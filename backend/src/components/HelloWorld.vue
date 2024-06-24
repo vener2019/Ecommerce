27af771fd4e02736dbbb6c1617406019ec31ffcd
Vener Dollisen <kulotsivener13@gmail.com>
2024-06-24 16:43:52 +0800

    Resolve merge conflicts

---
+++ backend/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, computed } from 'vue'
+import store from './../store';
+
+defineProps({
+  msg: String,
+})
+
+const test = computed(() => store.state.test)
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline bg-purple-500">
+    Hello world! {{ test }}
+  </h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
